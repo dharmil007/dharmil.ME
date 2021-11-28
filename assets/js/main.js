9324f42627efb1d14f36864f672784ed5478fa91
Dharmil Sanghvi <18544538+dharmil007@users.noreply.github.com>
2021-11-28 00:31:38 -0500

    v1

---
+++ assets/js/main.js
@@ -0,0 +1,103 @@
+
+$('body').scrollspy({ target: '.navmenu' });
+
+// When we click on the LI
+$("ul.qcontrols li").click(function(){
+  // If this isn't already active
+  if (!$(this).hasClass("active")) {
+    // Remove the class from anything that is active
+    $("ul.qcontrols li.active").removeClass("active");
+    // And make this active
+    $(this).addClass("active");
+  }
+});
+
+
+//WOW Scroll Spy
+var wow = new WOW({
+    //disabled for mobile
+    mobile: false
+});
+wow.init();
+
+jQuery(document).ready(function( $ ) {
+        $('.circle').circleProgress({
+        value: 0.70,
+        startAngle: 4.75,
+        size: 200,
+        fill: { gradient: ["#FFFFFF"] }
+      });
+        $('.two').circleProgress({
+          value: 0.75,
+        });
+        $('.three').circleProgress({
+          value: 0.50,
+        });
+        $('.four').circleProgress({
+          value: 0.90,
+        });
+
+   //Clients carousel Slider
+    $('#clients-carousel').owlCarousel({
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 400,
+        paginationSpeed: 800,
+        autoPlay: true,
+        pagination : false,
+        items : 4,
+        itemsCustom : false,
+        itemsDesktop : [1199,4],
+        itemsDesktopSmall : [980,3],
+        itemsTablet: [768,2],
+        itemsTabletSmall: false,
+        itemsMobile : [479,1],
+    });
+       $('#menu').click(function(){
+       $('#menu').not(this).removeClass("active");
+       $(this).toggleClass("active");
+      });
+
+  });
+
+  
+//ScrollTop
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+//collapseableElement
+$(function() {
+
+    var active = true;
+
+    $('#collapse-init').click(function() {
+        if (active) {
+            active = false;
+            $('.panel-collapse').collapse('show');
+            $('.panel-title').attr('data-toggle', '');
+            $(this).text('Enable accordion behavior');
+        } else {
+            active = true;
+            $('.panel-collapse').collapse('hide');
+            $('.panel-title').attr('data-toggle', 'collapse');
+            $(this).text('Disable accordion behavior');
+        }
+    });
+
+    $('#accordion').on('show.bs.collapse', function() {
+        if (active) $('#accordion .in').collapse('hide');
+    });
+
+});
+
